abd0beb94c644780fb939a07b856b067b03132cf
Vishal <Vishal@example.com>
2024-09-06 21:00:25 +0530

    Initial commit

---
+++ Hostel/src/test/java/com/vishal/hostel/managament/Hostel/HostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.hostel.managament.Hostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
